55a1efa065eda32d550979307a549297d0bb4b86
Xurnnba <1700229249@qq.com>
2023-06-28 01:10:27 +0800

    Scripts

--- My project/Assets/Scripts⭐           ️⭐️!!!!!!!!!!!!!!!!!!!!!!!!/CameraFollowsPlayer.cs
+++ My project/Assets/Scripts⭐           ️⭐️!!!!!!!!!!!!!!!!!!!!!!!!/CameraFollowsPlayer.cs
@@ -7,7 +7,7 @@ public class CameraFollowsPlayer : MonoBehaviour
     public Transform player;
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         transform.position = player.transform.position+new Vector3(14,17.5f,0);
     }
